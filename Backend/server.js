03d5b58628073c1d2a2aa52a32ce1a38c3b009a3
HarshThakur015 <tsayamonly@gmail.com>
2025-05-10 22:23:58 +0530

    Connecting Database

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send("API is running");
+});
+
+const port = 5000
+
+app.listen(5000, () => {
+  console.log(`Server running on port http://localhost:${port}`);
+});
