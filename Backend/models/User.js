154729d17caf0f5d55a34c23cb3c12b4a9ee8132
Harsh Thakur <tsayamonly@gmail.com>
2025-05-11 00:31:30 +0530

    Update User.js (added password in schema)

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -16,11 +16,17 @@ const userSchema = new mongoose.Schema({
       'Please fill a valid email address'
     ]
   },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'Password must be at least 6 characters long']
+  },
   favorites: {
     players: [String],
     clubs: [String],
     leagues: [String]
   }
 }, { timestamps: true });
+
 
 module.exports = mongoose.model('User', userSchema);
