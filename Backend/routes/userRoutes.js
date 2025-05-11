b8a79670f22c94a1ded50004c3db3d329858b95f
HarshThakur015 <tsayamonly@gmail.com>
2025-05-12 01:27:19 +0530

    GET API used

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -41,4 +41,20 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+router.get('/username/:username', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+
 module.exports = router;
