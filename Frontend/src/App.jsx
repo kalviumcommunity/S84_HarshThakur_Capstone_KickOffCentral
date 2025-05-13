4eb1af8988c7ee8ac4a333c8795c42ffb0f95250
HarshThakur015 <tsayamonly@gmail.com>
2025-05-13 05:59:14 +0530

    Testing Deployment

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <header>
         <h1>Welcome to Kickoff Central</h1>
         <p>Your one-stop solution for organizing and managing events</p>
-        <button>Get Started</button>
+        <button>Let's Go</button>
       </header>
     </div>
   );
