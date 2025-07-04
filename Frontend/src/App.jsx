62f6a1047824b5dc0dcada094b1785c5fbd7559e
HarshThakur015 <tsayamonly@gmail.com>
2025-07-05 01:03:55 +0530

    solved an issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+import LandingPage from './Pages/LandingPage';
 import './App.css';
-import Login from './Pages/Login';
+import Login from './Pages/Login'
 import Signup from './Pages/Signup';
-import Favourites from './pages/Favourites';
-import Home from './pages/Home';
+import Favourites from './Pages/Favourites';
+import Home from './Pages/Home';
 
 function App() {
   return (
